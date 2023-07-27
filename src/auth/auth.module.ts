543a3bbe065d204cdcbefd7e7bb5147555f326f8
dodinhphu <dodinhphu316.com>
2023-07-27 23:58:50 +0700

    [fix #30] [Logic] Update API login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,10 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './passport/local.strategy';
-import { jwtConstants } from './constants';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { AuthController } from './auth.controller';
+import ms from 'ms';
 
 @Module({
   imports: [
@@ -16,9 +16,9 @@ import { AuthController } from './auth.controller';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: jwtConstants.secret,
+        secret: configService.get<string>('TOKEN_SECRET'),
         signOptions: {
-          expiresIn: configService.get<string>('TIME_OUT_TOKEN'),
+          expiresIn: ms(configService.get<string>('TIME_OUT_TOKEN')),
         },
       }),
       inject: [ConfigService],
